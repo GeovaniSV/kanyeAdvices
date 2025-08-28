c17a968a843872167fb7d45943867a6aa63263c4
Geovani <geovani.020204@gmail.com>
2025-08-28 11:33:51 -0400

    getAdvice

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,48 @@
+import { useEffect, useState } from "react";
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { Button, StyleSheet, Text, View } from "react-native";
+
+//apps
+import { appAdvice } from "../services/api";
+
+interface IAdvice {
+  id: number;
+  advice: string;
+}
 
 export default function Home() {
+  const [Advice, setAdvice] = useState<IAdvice>();
+  const [isLoading, setIsLoading] = useState(false);
+
+  const getAdvice = async () => {
+    setIsLoading(true);
+    try {
+      const advice = await appAdvice.get("/advice");
+      setAdvice(advice.data.slip);
+      setIsLoading(false);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    getAdvice();
+  }, []);
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
+
+      <View style={styles.firstSection}>
+        <Text>Take an advice</Text>
+        <View style={styles.Advice}>
+          <Text>{isLoading ? "Loading..." : Advice?.advice}</Text>
+        </View>
+        <Button title="Change Advice" onPress={getAdvice} />
+      </View>
+
+      <View style={styles.secondSection}>
+        <Text>Alo2</Text>
+      </View>
     </View>
   );
 }
@@ -13,8 +50,33 @@ export default function Home() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
+    backgroundColor: "#e9e9e9",
+    alignItems: "center",
+    padding: 10,
+  },
+
+  firstSection: {
+    flex: 1,
     alignItems: "center",
     justifyContent: "center",
+    gap: 10,
+    width: "100%",
+    padding: 10,
+  },
+
+  Advice: {
+    backgroundColor: "#fff",
+    justifyContent: "center",
+    alignItems: "center",
+    width: "80%",
+    height: 35,
+    borderRadius: 35,
+    padding: 35,
+    boxShadow: "0 4px 8px rgba(0, 0, 0, 0.1)",
+  },
+
+  secondSection: {
+    flex: 1,
+    width: "100%",
   },
 });
